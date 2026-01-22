64f1b6729d03aca4a593deacedc567dac2c4cb11
FEOvids <omermok9@gmail.com>
2026-01-22 11:08:45 +0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 function yap() {
   alert("you clicked it. happy now?");
 }
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.register("service-worker.js")
+    .then(() => console.log("service worker locked in"))
+    .catch(err => console.log("sw failed", err));
+}
